6a0b487fd0d0a2a740b92292992f1982e6ee25a2
Peter van Wesep <petervanwesep@gmail.com>
2016-01-26 15:28:09 -0800

    change copy

--- app/assets/javascripts/components/frontPageCopy.jsx
+++ app/assets/javascripts/components/frontPageCopy.jsx
@@ -1,6 +1,3 @@
-open
-
-
 var FrontPageCopy = React.createClass({
   getInitialState: function() {
     if(window.location.hash) {
@@ -88,10 +85,10 @@ var Code = React.createClass({
         <p>Unlike most professions, coding lets me create digital worlds out of a few hundred lines of text. Unfortunately, it also lets me create entire worlds of pain in even fewer.</p>
         <p>I've adopted a few guidelines that help me stay (for the most part) in the former camp:</p>
         <ol>
-          <li>Code should be well-tested and easy to maintain. While acquiring technical debt may be necessary for rapid developement, it should be paid off as soon as possible.</li>
+          <li>Code should be well-tested and easy to maintain. While adding technical debt may be necessary for rapid developement, it should be paid off as soon as possible.</li>
           <li>Release cycles should be kept short, thus providing the team with a clear focus and giving stakeholders an immediate opportunity for feedback.</li>
-          <li>Teams should be small and tightly knit. Code should be paired on whenever possible. One phrase that has stuck with me is to consider "people as your product". Without highly collaborative teams who are passionate about their product, it is impossible to achieve the best results.</li>
-          <li>And finally, ideas that do not lead to the best results should be abandoned for better ideas. It may sound obvious, but bad results are settled for often enough that it bears articulating.</li>
+          <li>Teams should be small and tightly knit. Code should be paired on whenever possible. One philosophy that has stuck with me is to consider "people as your product". Without highly collaborative teams who are passionate about their product, it is impossible to achieve the best results.</li>
+          <li>And finally, ideas that don't lead to the best results should be abandoned for better ideas.</li>
         </ol>
       </section>
     );
@@ -103,8 +100,8 @@ var K12 = React.createClass({
     return(
       <section className='content'>
         <p>I've spent a large part of my life, in one form or another, pursuing my love of teaching.</p>
-        <p>From giving piano lessons in high school, to pursuing a music education degree at Indiana University, to my work teaching after-school robotics with the Seattle YMCA, to my current role with <a href="http://tealsk12.org">TEALS</a> teaching 1st period computer science at Skyline High School in Oakland, I have always found joy interacting with and teaching kids.</p>
-        <p>I am an advocate of the <a href="https://en.wikipedia.org/wiki/Small_schools_movement">small schools movement</a> and <a href="https://en.wikipedia.org/wiki/Project-based_learning">project-based</a> approaches to learning. I am an occasional critic of the Common Core State Standards, some aspects of charter schools (<a href="http://dianeravitch.net/2012/08/12/the-charter-school-threat/">mostly this one</a>) and any argument against increased funding for public education that ignores our country's unique and increasingly diverse ethnic, cultural and socioeconomic demographics.</p>
+        <p>From giving piano lessons in high school, to pursuing a music education degree at Indiana University, to my work teaching after-school robotics with the Seattle YMCA, to my current role with <a href="http://tealsk12.org" target="_blank">TEALS</a> teaching 1st period computer science at Skyline High School in Oakland, I have always found joy interacting with and teaching kids.</p>
+        <p>I am an advocate of the <a href="https://en.wikipedia.org/wiki/Small_schools_movement" target="_blank">small schools movement</a> and <a href="https://en.wikipedia.org/wiki/Project-based_learning" target="_blank">project-based</a> approaches to learning. I am an occasional critic of the Common Core State Standards, some aspects of charter schools (<a href="http://dianeravitch.net/2012/08/12/the-charter-school-threat/" target="_blank">mostly this one</a>) and any argument against increased funding for public education that ignores our country's unique and increasingly diverse ethnic, cultural and socioeconomic demographics.</p>
         <p>Mostly though, I just try to contribute in a way that seems helpful. I'll talk to teachers, administrators and students in an effort to figure out what that is.</p>
       </section>
     );
