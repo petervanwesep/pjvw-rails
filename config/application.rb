0faaf13e24f439bcb863e300cd7cd258b591a235
Peter van Wesep <petervanwesep@gmail.com>
2018-09-12 15:01:30 -0700

    force ssl

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module PjvwRails
   class Application < Rails::Application
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     config.autoload_paths += %W(#{config.root}/lib)
+    config.force_ssl = true if Rails.env.production?
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
